be7cf0d46a0000b23460df71087b8410ce742777
Simcha Kanter <simchakanter@gmail.com>
2014-07-13 20:05:32 -0500

    Refactor the gender pronoun function into the model

--- js/app.js
+++ js/app.js
@@ -9,24 +9,14 @@ angular.module('app', [])
       useless_skill: "",
       adjective: "",
       obnoxious_celebrity: "",
-      huge_number: ""
-    };
-    $scope.gender = "male";
-    $scope.selectGender = function(typeOfWord) {
-      if ($scope.gender === "male") {
-        return (typeOfWord === "pronoun") ? "he":"his";
-      } else {
-        return (typeOfWord === "pronoun") ? "she":"her";
+      huge_number: "",
+      gender: "male",
+      selectGender: function(typeOfWord) {
+        if (this.gender === "male") {
+          return (typeOfWord === "pronoun") ? "he":"his";
+        } else {
+          return (typeOfWord === "pronoun") ? "she":"her";
+        }
       }
     };
   });
-
-
-// I attempted to add a function in here to update the pronoun,
-// but it wasn't getting called when I changed the radio button.
-// Instead I made the terribly ugly and unDRY ng-if thing.
-// It wasn't worth it.
-
-// something like adding gender_pronoun to the $scope.data object
-// then creating a function that checks the value of gender an
-// sets the pronoun.
