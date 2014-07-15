a4e468fba0e8937936d6d3f0f4677daedd3c6e4c
Simcha Kanter <simchakanter@gmail.com>
2014-07-15 16:23:28 -0500

    Finish submission form and two-step process

--- js/app.js
+++ js/app.js
@@ -13,10 +13,41 @@ angular.module('app', [])
       gender: "male",
       selectGender: function(typeOfWord) {
         if (this.gender === "male") {
-          return (typeOfWord === "pronoun") ? "he":"his";
+          return (typeOfWord === "pronoun") ? "he":"him";
         } else {
           return (typeOfWord === "pronoun") ? "she":"her";
         }
       }
+    };
+    $scope.submit = function() {
+      if ($scope.madLibsForm.$valid) {
+        this.validationPassed = true;
+      }
+    };
+    $scope.reset = function() {
+      console.log("Reset function ran");
+      // I know that I shouldn't have this again, but not sure
+      // how to store it so it won't get updated.
+      this.data = {
+        name: "",
+        job_title: "",
+        tedious_task: "",
+        dirty_task: "",
+        celebrity: "",
+        useless_skill: "",
+        adjective: "",
+        obnoxious_celebrity: "",
+        huge_number: "",
+        gender: "male",
+        selectGender: function(typeOfWord) {
+          if (this.gender === "male") {
+            return (typeOfWord === "pronoun") ? "he":"his";
+          } else {
+            return (typeOfWord === "pronoun") ? "she":"her";
+          }
+        }
+      };
+      this.validationPassed = false;
+      this.submitted = false;
     };
   });
